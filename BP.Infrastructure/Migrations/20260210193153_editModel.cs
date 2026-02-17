77a38230bf23cb924144f48587da74bd0460839f
SandroGitA <maktavish74@mail.ru>
2026-02-17 18:58:19 +0500

    add docker-compose file

---
+++ BP.Infrastructure/Migrations/20260210193153_editModel.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BP.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class editModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Operators",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Operations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Accounts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Operators");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Operations");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Accounts");
+        }
+    }
+}
